1f4a60093dc683c42237d75e09f21e61de39d750
Victor-Eliseo-Luzuriaga <86174810+Victor-Eliseo-Luzuriaga@users.noreply.github.com>
2021-07-17 21:40:17 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import ListCards from './components/cards/ListCards';
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <ListCards></ListCards>
+    </div>
+  );
+}
+
+export default App;
